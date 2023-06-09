ba2f6d6dfe9f3d583edde47d9e5b1e427407dbba
olliv <olliv@DESKTOP-FNCKMIA>
2023-06-10 01:01:55 +0200

    Got the MoveFiles script to work, it now checks if the file is one of the target directory folders and skips if so.

--- MoveFiles.ps1
+++ MoveFiles.ps1
@@ -4,7 +4,20 @@
 
 #Requires -RunAsAdministrator
 param(
-    $SourceDir = "C:\PStemp",
-    $TargetDir = "C:\PStemp\Result"
+    [string]$SourceDir = "C:\PStemp",
+    [string]$TargetDir = "C:\PStemp\Result"
 )
-Move-Item $SourceDir $TargetDir
+foreach ($File in Get-ChildItem($SourceDir )) {
+    $TargetFolders = $TargetDir.Split('\')
+    $MoveFile = 1
+    foreach($Folder in $TargetFolders){ #Check so we dont try and move one of the folders of the target directory
+        if ($Folder -eq $File){
+            $MoveFile = 0
+            break
+        }
+    }
+    if ($MoveFile -eq 1) {
+        Move-Item ($SourceDir+$File) $TargetDir
+        Write-Host $File.Name 'Moved'
+    }
+}
