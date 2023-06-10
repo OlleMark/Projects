1a28c6521d7306cf033b0f5de823e922faf8c2d6
olliv <olliv@DESKTOP-FNCKMIA>
2023-06-10 02:46:15 +0200

    Removed the need to have MoveFiles.ps1 in the same directory because it is not used

--- SortDesktop.ps1
+++ SortDesktop.ps1
@@ -4,13 +4,6 @@ param(
     $DesktopDir
 )
 
-if (Test-Path .\MoveFiles.ps1) {
-    Write-Host 'MoveFiles.ps1 Exists'
-}
-else {
-    throw 'MoveFiles.ps1 not found in the same directory'
-}
-
 $AllFiles = Get-ChildItem($DesktopDir)
 $SortedDictionary = New-Object System.Collections.Generic.Dictionary"[string,System.Collections.Generic.List[String]]"
 
